fbd619369bd96e4359523c0ce32570e92987122c
Ichsan Haikal <111943770+IchsanHaikal@users.noreply.github.com>
2023-10-07 14:45:00 +0700

    Update dashboard.py

--- dicoding/dashboard.py
+++ dicoding/dashboard.py
@@ -21,28 +21,28 @@ if selected_option == "Distribusi Menurut Musim":
     st.header("Distribusi Sewa Sepeda Menurut Musim")
 
     # Visualisasi distribusi jumlah rental bike berdasarkan musim
-    plt.figure(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 6))
     sns.boxplot(
         x="season", y="cnt", data=data, order=["Spring", "Summer", "Fall", "Winter"]
     )
     plt.title("Distribusi Jumlah Rental Bike Berdasarkan Musim")
     plt.xlabel("Musim")
     plt.ylabel("Jumlah Rental Bike")
-    plt.show()
+    st.pyplot(fig)
 
 
 elif selected_option == "Perbandingan antara Hari Kerja dan Hari Libur":
     st.header("Perbandingan Rata-rata Sewa Sepeda antara Hari Kerja dan Hari Libur")
 
     # Visualisasi perbandingan rata-rata jumlah rental bike antara hari kerja dan hari libur
-    plt.figure(figsize=(8, 5))
+    fig, ax = plt.subplots(figsize=(8, 5))
     sns.barplot(x="workingday", y="cnt", data=data)
     plt.title(
         "Perbandingan Rata-rata Jumlah Rental Bike antara Hari Kerja dan Hari Libur"
     )
     plt.xlabel("Hari Kerja (0: Hari Libur, 1: Hari Kerja)")
     plt.ylabel("Rata-rata Jumlah Rental Bike")
-    plt.show()
+    st.pyplot(fig)
 
 # Menampilkan tabel data
 st.header("Tabel Data")
